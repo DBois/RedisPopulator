95813a84ae895be790df0229a709718b74408b6e
Adam Saidane <adamsaidane@hotmail.com>
2020-05-08 19:57:44 +0200

    Refactored project to be modular

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using StackExchange.Redis;
+
+namespace RedisPopulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read File
+            // TODO: Change this to match the path of your data.csv file
+            const string pathToData = @"D:\Github\Software Development\Database\Comparisons\data.csv";
+            var lines = File.ReadAllLines(pathToData);
+            
+            var muxer = ConnectionMultiplexer.Connect("localhost");
+            // Get a connection to database 1. You can leave this parameter blank or select another db to connect to.
+            var conn = muxer.GetDatabase(1);
+            
+            // Split the CSV line into a string array, and then select all indices but the first element.
+            // The first element should be the Key you want in Redis.
+            var headers = lines[0].Split(',');
+
+            // Array which will contain the key value entries.
+            var entries = new HashEntry[headers.Length - 1];
+            
+            var numberOfElements = (lines.Length - 1).ToString("N0");
+            Console.WriteLine($"Writing {numberOfElements} elements to DB please wait...");
+            for (var i = 1; i < lines.Length; i++)
+            {
+                var values = lines[i].Split(',');
+                // Assemble hash entries (Key, Values) to be inserted into the hash
+                for (var j = 1; j < headers.Length; j++)
+                {
+                    entries[j - 1] = new HashEntry(headers[j], values[j]);
+                }
+                // Query the entries to the DB
+                conn.HashSet($"{headers[0]}:{values[0]}", entries);
+            }
+
+            Console.WriteLine("Finished!");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Redis Data Populator
+This is just a simple program to populate your redis database with data and headers from a CSV file.  
+By default, this code will use elements at the first indices as the key for the hashes.  
+```
+InvoiceNo,StockCode,Description,Quantity,InvoiceDate,UnitPrice,CustomerID,Country
+536365,85123A,WHITE HANGING HEART T-LIGHT HOLDER,6,12/1/2010 8:26,2.55,17850,United Kingdom
+```
+If your CSV data looks like the above, they keys will look like this:  
+`InvoiceNo:536365` and the key will store the rest of the data in a hash.  
